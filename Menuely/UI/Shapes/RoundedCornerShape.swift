a288536bac9012d9a93447bbc6f2b363f9513099
Benjamin Mecanović <benjaminmecanovic@Benjamins-MacBook-Pro.local>
2021-07-26 14:48:45 +0200

    feat: search cell

---
+++ Menuely/UI/Shapes/RoundedCornerShape.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedCornerShape.swift
+//  Menuely
+//
+//  Created by Benjamin Mecanović on 26.07.2021..
+//
+
+import SwiftUI
+
+struct RoundedCornersShape: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
