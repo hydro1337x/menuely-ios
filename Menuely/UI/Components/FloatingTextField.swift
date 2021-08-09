fb3453cb45e399baf1b01adda159d182d1178a65
Benjamin Mecanović <hydrobf3@gmail.com>
2021-08-09 22:57:44 +0200

    refactor: floating textfield

--- Menuely/UI/Components/FloatingTextField.swift
+++ Menuely/UI/Components/FloatingTextField.swift
@@ -33,33 +33,31 @@ struct FloatingTextField: View {
     
     var body: some View {
        ZStack(alignment: .leading) {
-        GeometryReader { geometry in
-                Text(title)
-                    .font(.body)
-                    .foregroundColor(placeholderColor)
-                    .offset(y: placeholderOffset)
-                    .scaleEffect(placeholderScale, anchor: .leading)
-                
-                TextField("", text: $text) { isActive in
-                    withAnimation(.spring(response: 0.3, dampingFraction: 0.5)) {
-                        separatorColor = isActive ? Color(#colorLiteral(red: 0.2075126171, green: 0.7053237557, blue: 0.3391282558, alpha: 1)) : Color(#colorLiteral(red: 0.7803257108, green: 0.7804361582, blue: 0.7802907825, alpha: 1))
-                    }
-                }
-                .font(.body)
-                .foregroundColor(Color(#colorLiteral(red: 0.2980110943, green: 0.2980577946, blue: 0.2979964018, alpha: 1)))
-                .onChange(of: text, perform: { value in
-                    withAnimation(.spring(response: 0.3, dampingFraction: 0.5)) {
-                        placeholderColor = value.isEmpty ? Color(#colorLiteral(red: 0.7803257108, green: 0.7804361582, blue: 0.7802907825, alpha: 1)) : Color(#colorLiteral(red: 0.2075126171, green: 0.7053237557, blue: 0.3391282558, alpha: 1))
-                        placeholderOffset = value.isEmpty ? 0 : -25
-                        placeholderScale = value.isEmpty ? 1 : 0.75
-                    }
-                })
-                
-                RoundedRectangle(cornerRadius: 2, style: .continuous)
-                    .foregroundColor(separatorColor )
-                    .frame(width: geometry.size.width, height: 2, alignment: .center)
-                    .offset(x: 0, y: 25)
+        Text(title)
+            .font(.body)
+            .foregroundColor(placeholderColor)
+            .offset(y: placeholderOffset)
+            .scaleEffect(placeholderScale, anchor: .leading)
+        
+        TextField("", text: $text) { isActive in
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.5)) {
+                separatorColor = isActive ? Color(#colorLiteral(red: 0.2075126171, green: 0.7053237557, blue: 0.3391282558, alpha: 1)) : Color(#colorLiteral(red: 0.7803257108, green: 0.7804361582, blue: 0.7802907825, alpha: 1))
             }
+        }
+        .frame(maxHeight: .infinity)
+        .font(.body)
+        .foregroundColor(Color(#colorLiteral(red: 0.2980110943, green: 0.2980577946, blue: 0.2979964018, alpha: 1)))
+        .onChange(of: text, perform: { value in
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.5)) {
+                placeholderColor = value.isEmpty ? Color(#colorLiteral(red: 0.7803257108, green: 0.7804361582, blue: 0.7802907825, alpha: 1)) : Color(#colorLiteral(red: 0.2075126171, green: 0.7053237557, blue: 0.3391282558, alpha: 1))
+                placeholderOffset = value.isEmpty ? 0 : -25
+                placeholderScale = value.isEmpty ? 1 : 0.75
+            }
+        })
+        
+        Divider()
+            .foregroundColor(separatorColor)
+            .offset(x: 0, y: 20)
        }
        .padding(.top, 15)
    }
